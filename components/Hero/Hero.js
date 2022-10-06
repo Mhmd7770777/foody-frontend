2a7f62ab538cd37b98afe4cb8df4628974ad1901
Mohammad <mohammad.hijazi.2000@gmailcom>
2022-10-06 22:40:26 +0300

    completed hero component

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,4 +1,4 @@
-import { Box, Container, Grid, styled } from "@mui/material";
+import { Box, Container, Grid, styled, Typography } from "@mui/material";
 
 const Hero = () => {
   const StyledCard = styled(Box)({
@@ -8,21 +8,53 @@ const Hero = () => {
     backgroundPosition: "center",
     backgroundSize: "cover",
     height: 400,
+    cursor: "pointer",
+    "&:hover": {
+      opacity: 0.8,
+      boxSizing: "borderBox",
+      zIndex: 1,
+      transition: "all 0.45s ease",
+    },
   });
+
+  const StyledTypography = styled(Typography)({
+    textAlign: "center",
+    color: "white",
+    background: "tomato",
+    fontSize: 20,
+  });
+
+  const StyledWrapper = styled(Box)({
+    paddingTop: "140%",
+    width: "80%",
+  });
+
   return (
     <Container>
       <Grid container direction={"row"} columnSpacing={2} rowSpacing={2}>
         <Grid item xs={6} md={3}>
           <StyledCard sx={{ backgroundImage: `url()` }}></StyledCard>
+          <StyledWrapper>
+            <StyledTypography>Burger</StyledTypography>
+          </StyledWrapper>
         </Grid>
         <Grid item xs={6} md={3}>
           <StyledCard sx={{ backgroundImage: `url()` }}></StyledCard>
+          <StyledWrapper>
+            <StyledTypography>Burger</StyledTypography>
+          </StyledWrapper>
         </Grid>
         <Grid item xs={6} md={3}>
           <StyledCard sx={{ backgroundImage: `url()` }}></StyledCard>
+          <StyledWrapper>
+            <StyledTypography>Burger</StyledTypography>
+          </StyledWrapper>
         </Grid>
         <Grid item xs={6} md={3}>
           <StyledCard sx={{ backgroundImage: `url()` }}></StyledCard>
+          <StyledWrapper>
+            <StyledTypography>Burger</StyledTypography>
+          </StyledWrapper>
         </Grid>
       </Grid>
     </Container>
